c9bb2b3a3224bf2c1d50fc5274d7439d760d3832
mavery735@gmail.com <mavery735@gmail.com>
2025-12-25 02:42:54 -0500

    fixed some more issues that came up and swapped to vs-jetpack for qtgmc

--- modules/getfilters.lua
+++ modules/getfilters.lua
@@ -1,23 +1,5 @@
 local args = require("modules/args") --gets arguments
 local getfps = require("modules/getfps")
-local deinterlace_presets = {
-	'"Placebo"',
-	'"Very Slow"',
-	'"Slower"',
-	'"Slow"',
-	'"Medium"',
-	'"Fast"',
-	'"Faster"',
-	'"Very Fast"',
-	'"Super Fast"',
-	'"Ultra Fast"',
-	'"Draft"',
-} --qtgmc preset, 1-11
-local deinterlace_field_orders = {
-	-1, --vapoursynth default
-	0, --bottom field first
-	1, --top field first
-} --qtgmc field order 1-3
 local ivtc_slows = {
 	2, --slow
 	1, --medium
@@ -32,16 +14,6 @@ local deinterlace_preset
 local deinterlace_field_order
 local ivtc_slow
 local ivtc_field_order
-if args.deinterlace_preset then
-	deinterlace_preset = deinterlace_presets[tonumber(args.deinterlace_preset)]
-else
-	deinterlace_preset = deinterlace_presets[3]
-end
-if args.deintelace_field then
-	deinterlace_field_order = deinterlace_field_orders[tonumber(args.deintelace_field)]
-else
-	deinterlace_field_order = deinterlace_field_orders[1]
-end
 if args.ivtc_slow then
 	ivtc_slow = ivtc_slows[tonumber(args.ivtc_slow)]
 else
@@ -66,11 +38,7 @@ local function getfilters(input)
 				.. vsscripts[3]
 				.. ' -c y4m -a video="'
 				.. input
-				.. '" -a deinterlace_preset='
-				.. deinterlace_preset
-				.. " -a deinterlace_field_order="
-				.. deinterlace_field_order
-				.. " -a ivtc_slow="
+				.. '" -a ivtc_slow='
 				.. ivtc_slow
 				.. " -a ivtc_field_order="
 				.. ivtc_field_order
@@ -98,11 +66,7 @@ local function getfilters(input)
 				.. vsscripts[2]
 				.. ' -c y4m -a video="'
 				.. input
-				.. '" -a deinterlace_preset='
-				.. deinterlace_preset
-				.. " -a deinterlace_field_order="
-				.. deinterlace_field_order
-				.. " -a fps_divisor="
+				.. '" -a fps_divisor='
 				.. fps.fpsdivisortxt
 				.. " -a fps_dividend="
 				.. fps.fpsdividendtxt
--- modules/getfps.lua
+++ modules/getfps.lua
@@ -2,6 +2,7 @@ local fpsdividendtxt
 local fpsdivisortxt
 local fps
 local fpstable
+local fpsparsed
 local function getfps(input)
 	local fpscmd = io.popen(
 		'ffprobe -v error -select_streams v:0 -show_entries stream=avg_frame_rate -of default=noprint_wrappers=1:nokey=1 "'
@@ -9,16 +10,19 @@ local function getfps(input)
 			.. '"'
 	) --command to get the fps of the source video
 	fpsparsed = fpscmd:read("*a") --reads the fps
+	print(fpsparsed)
 	if fpsparsed then
 		fpsdividendtxt, fpsdivisortxt = string.match(fpsparsed, "(.*)%/(.*)") --filters the fps so it is only the number for certain
-		fpsdividendtxt = string.match(fpsdividendtxt, "%d+")
-		fpsdivisortxt = string.match(fpsdivisortxt, "%d+")
-		fps = fpsdividendtxt .. "/" .. fpsdivisortxt
-		fpstable = {
-			fps = fps,
-			fpsdividendtxt = fpsdividendtxt,
-			fpsdivisortxt = fpsdivisortxt,
-		}
+		if fpsdividendtxt and fpsdivisortxt then
+			fpsdividendtxt = string.match(fpsdividendtxt, "%d+")
+			fpsdivisortxt = string.match(fpsdivisortxt, "%d+")
+			fps = fpsdividendtxt .. "/" .. fpsdivisortxt
+			fpstable = {
+				fps = fps,
+				fpsdividendtxt = fpsdividendtxt,
+				fpsdivisortxt = fpsdivisortxt,
+			}
+		end
 	end
 	if fpstable then
 		return fpstable
