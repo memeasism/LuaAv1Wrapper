2c04c98c12dfaac9840e63b3c2c8577756654a7d
Memeasism <82785402+memeasism@users.noreply.github.com>
2025-12-25 03:29:42 -0500

    Update args.lua

--- modules/args.lua
+++ modules/args.lua
@@ -10,12 +10,6 @@ parser:option():name("-q --videoquality"):description("cqp value 0-255, lower is
 parser:option():name("-b --audiobitrate"):description("actual audio bitrate in bits or for kbps as such: 128k")
 parser:option():name("-n --noise"):description("CPU ONLY! value 0-50 0=off will denoise and do grain synthesis")
 parser:flag("-l --interlaced", "your video will be deinterlaced.")
-parser:option("--deinterlace_field"):description(
-	"set field order for deinterlacing, options: 1-3, 1 is default/automatic, 2 is bottom field first, 3 is top field first."
-)
-parser:option("--deinterlace_preset"):description(
-	"set the preset used for deinterlacing, options 1-11, 3 is default, 1 is slowest but best quality, 11 is fastest and worst quality."
-)
 parser:flag("-t --telecine", "your video will be detelecined.")
 parser:option("--ivtc_field"):description(
 	"set field order for inverse telecine, options: 1-3, 1 is default/automatic, 2 is bottom field first, 3 is top field first."
