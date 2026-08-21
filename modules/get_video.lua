974a86053817fe5106dd1934c7981b2ec7b1cff9
Michael Avery <mavery735@gmail.com>
2026-08-20 23:09:34 -0400

    fixed if you dont compile libaom with vmaf

--- modules/get_video.lua
+++ modules/get_video.lua
@@ -99,7 +99,7 @@ local function getvideo(
 	local function cpu_command(out, target_vmaf, quality)
 		local command
 		command = string.format(
-			[[av1an -i %s --proxy %s -o "%s" --workers %s -e "aom" --pix-format yuv420p10le --scenes %s --target-metric vmaf --target-quality %s -v "--end-usage=q --denoise-noise-level=%s --threads=2 --cpu-used=3 --row-mt=1 --good --passes=2 --tune=vmaf --tune-content=default --min-partition-size=16 --max-partition-size=128 --aq-mode=1 --quant-b-adapt=1 --enable-qm=1"]],
+			[[av1an -i %s --proxy %s -o "%s" --workers %s -e "aom" --pix-format yuv420p10le --scenes %s --target-metric vmaf --target-quality %s -v "--end-usage=q --denoise-noise-level=%s --threads=2 --cpu-used=4 --row-mt=1 --good --passes=2 --tune-content=default --min-partition-size=16 --max-partition-size=128 --aq-mode=1 --quant-b-adapt=1 --enable-qm=1"]],
 			filters.av1an,
 			filters.proxy.av1an,
 			out,
